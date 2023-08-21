1d038a658ef1bb257e146cb106d5d10c0428851e
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-08-21 12:28:55 -0400

    Working Nav Bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,55 +1,18 @@
-import React, { useState } from 'react';
-import { Navbar, NavbarBrand, Container, NavbarToggler, Collapse, NavbarNav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, DropdownDivider, Form, FormInput, Button } from 'react-bootstrap';
-import './login.css';
+import React from 'react';
+import './home.css'; // Make sure to create this CSS file
 
-const Home = (props) => {
-  const [visible, setVisible] = useState(false);
-
+const Home = () => {
   return (
-    <div>
-      <h1>Hello from Home</h1>
-      <Navbar expand="lg" bg="light">
-        <Container fluid>
-          <NavbarBrand href="#">Navbar</NavbarBrand>
-          <NavbarToggler onClick={() => setVisible(!visible)} />
-          <Collapse className="navbar-collapse" in={visible}>
-            <NavbarNav>
-              <NavItem>
-                <NavLink href="#" active>
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Link</NavLink>
-              </NavItem>
-              <Dropdown as={NavItem} variant="nav-item">
-                <Dropdown.Toggle as={NavLink} variant="secondary">
-                  Dropdown button
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#">Action</Dropdown.Item>
-                  <Dropdown.Item href="#">Another action</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <NavItem>
-                <NavLink href="#" disabled>
-                  Disabled
-                </NavLink>
-              </NavItem>
-            </NavbarNav>
-            <Form className="d-flex">
-              <FormInput type="search" className="me-2" placeholder="Search" />
-              <Button type="submit" variant="outline-success">
-                Search
-              </Button>
-            </Form>
-          </Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">My Website</div>
+      <ul className="navbar-links">
+        <li className="navbar-link">Home</li>
+        <li className="navbar-link">About</li>
+        <li className="navbar-link">Services</li>
+        <li className="navbar-link">Contact</li>
+      </ul>
+    </nav>
   );
-}
+};
 
 export default Home;
